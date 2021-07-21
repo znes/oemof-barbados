94dd3ff16534224871afd0595d01ec40d2dad7f5
simnh <simon.hilpert@uni-flensburg.de>
2021-07-21 10:44:30 +0200

    Fix cruise ship demand

--- scripts/cruise-demand.py
+++ scripts/cruise-demand.py
@@ -14,7 +14,7 @@ df["ARRIVAL"] =  pd.to_datetime(df["ARRIVED DATE"] + " " + df["TIME"] )
 df["DEPATURE"] = df["ARRIVAL"] + pd.to_timedelta(df['HOURS'],"h")
 
 # create function to interpolate based on relative time length stay of ship
-demand = np.array([15, 15, 14, 10, 4, 4, 4, 4, 10, 14, 15, 15])
+demand = np.array([12, 12, 10, 6, 4, 4, 4, 4, 6, 10, 12, 12])
 timerel = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])/11
 f = interpolate.interp1d(timerel, demand)
 
@@ -37,7 +37,7 @@ demand_agg_sum = demand_agg_sum.reindex(pd.date_range(start="2018", periods=8760
 ax = demand_agg_sum.plot()
 ax.set_ylabel("Aggregated Cruise Ship Demand in MW")
 plt.savefig("visualization/input-cruise-ship-demand.pdf")
-# to see the structure of the arrivals and depatures 
+# to see the structure of the arrivals and depatures
 df["ARRIVAL TIME"] = pd.to_timedelta(df["TIME"]) / pd.offsets.Hour(1) #.astype("float")
 df["HOURS OF STAY"] = pd.to_timedelta(df["HOURS"]) / pd.offsets.Hour(1) #.astype("float")
 df.plot.scatter(x="ARRIVAL TIME", y="HOURS OF STAY")
